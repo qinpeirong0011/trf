87debfe73b2cc4653ebb438ef503b342f7b457f5
qinpr <peirongq_1@tujia.com>
2018-10-09 11:34:35 +0800

    cm

--- trf-config/trf-config-spring/src/main/java/com/qinpr/trf/config/spring/schema/TrfNamespaceHandler.java
+++ trf-config/trf-config-spring/src/main/java/com/qinpr/trf/config/spring/schema/TrfNamespaceHandler.java
@@ -1,12 +1,13 @@
-//package com.qinpr.trf.config.spring.schema;
-//
-//import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
-//
-///**
-// * Created by qinpr on 18/4/26.
-// */
-//public class TrfNamespaceHandler extends NamespaceHandlerSupport {
-//    public void init() {
-//        System.out.println("init ..... TrfNamespaceHandler");
-//    }
-//}
+package com.qinpr.trf.config.spring.schema;
+
+import com.qinpr.trf.config.RegistryConfig;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * Created by qinpr on 2018/10/8.
+ */
+public class TrfNamespaceHandler extends NamespaceHandlerSupport {
+    public void init() {
+        registerBeanDefinitionParser("registry", new TrfBeanDefinitionParser());
+    }
+}
---
+++ trf-test/trf-test-spring/src/main/java/com/qinpr/trf/test/spring/consumer/ConsumerConfiguration.java
@@ -0,0 +1,28 @@
+package com.qinpr.trf.test.spring.consumer;
+
+import com.qinpr.trf.config.RegistryConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by qinpr on 2018/10/9.
+ */
+@ContextConfiguration(locations = { "classpath:trf-provider.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ConsumerConfiguration {
+
+    @Autowired
+    @Qualifier("test")
+    private RegistryConfig test;
+
+    @Test
+    public void init() {
+        System.out.println(test.getAddress());
+        System.out.println(test.getProtocol());
+        System.out.println(test.getGroup());
+    }
+}
